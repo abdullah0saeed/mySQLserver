7c09a4b421f49dd4da2804930eb13ece57064a95
Abdullah Saeed <abdullahhamouda056@gmail.com>
2023-05-28 22:56:43 +0300

    2nd

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { db } = require("./config/db");
 const insertRouter = require("./routes/insert");
 const queryRouter = require("./routes/query");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/insert", insertRouter);
--- routes/query.js
+++ routes/query.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { query } = require("../controllers/select");
+const { query } = require("../controllers/query");
 
 router.post("/", query);
 
