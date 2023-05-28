72817915864b57edf0cf9895764720b3d4c7ebf9
Abdullah Saeed <abdullahhamouda056@gmail.com>
2023-05-29 02:38:09 +0300

    add error handling

--- controllers/query.js
+++ controllers/query.js
@@ -5,6 +5,7 @@ exports.query = async (req, res) => {
   const { query } = req.body;
   try {
     await connection.query(query, (error, result, fields) => {
+      if (error) return res.status(400).send(error);
       return res.send(result);
     });
   } catch (error) {
